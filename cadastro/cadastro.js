37bb36c9c98eb31e5951267bdc55b2ee15a1dc91
Unknown <lucaaamaral@gmail.com>
2018-10-18 18:03:27 +0000

    Criado javascript de cadastro

---
+++ cadastro/cadastro.js
@@ -0,0 +1,46 @@
+var formulario = document.getElementsByClassName("form");
+//.onfocus;// = focused(this);
+//document.getElementsByClass("form").onblur = focusout(this);
+var i, k = formulario.length;
+for (i=0; i<k; i++)
+{
+	if(formulario[i].type == "text")
+	{
+		formulario[i].onfocus = function onfocus(event) {focused(this)}
+		formulario[i].onblur = function onblur(event) {focusout(this)}
+		formulario[i].value = formulario[i].id
+		//formu[i].value = formu[i].onfocus;
+	}
+}
+
+function focused(obj)
+{
+	if (obj.value == obj.id)
+	{
+		obj.value = '';
+	}
+}
+
+function focusout(obj)
+{
+	if (obj.value == '')
+	{
+		obj.value = obj.id;
+	}
+}
+
+function submit_db()
+{
+	for (i=0; i<k-1; i++)
+	{
+		if(formulario[i].type == "text" && formulario[i].id == formulario[i].value)
+		{
+			formulario[i].value = null;
+		}
+	}
+
+	document.getElementById("formulario").reset();
+//enviar ao banco de dados
+
+}
+
