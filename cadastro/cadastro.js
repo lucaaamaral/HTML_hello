65279fe1318edea72694290f3ef01bace6b733ff
Unknown <lucaaamaral@gmail.com>
2018-10-23 17:56:00 +0000

    Gravação com localstorage concluida
    Futuras verificações ainda são necessárias

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -6,6 +6,10 @@ for (i=0; i<k; i++)
 {
 	if(formulario[i].type == "text")
 	{
+		//if (sessionstorage.origem)
+		{
+			//document.getElementById("Origem").innerHTML = sessionstorage.origem
+		}
 		formulario[i].onfocus = function onfocus(event) {focused(this)}
 		formulario[i].onblur = function onblur(event) {focusout(this)}
 		formulario[i].value = formulario[i].id
@@ -32,6 +36,7 @@ function focusout(obj)
 
 function show_grupo()
 {
+	//document.getElementById("ver").innerHTML = "show_grupo()"
 	if (document.getElementById("categoria").value == "C")
 	{
 		document.getElementById("ver").innerHTML = "C"
@@ -47,43 +52,45 @@ function show_grupo()
 
 function local_save()
 {
-	
 	if(localStorage.requisition)
 	{
-		//document.getElementById("ver").innerHTML = Number(localStorage.requisition)
 		localStorage.requisition = Number(localStorage.requisition)+1
 	}
-	else
+	
+	if (	document.getElementById("Referencia modelo").value	== "Referencia modelo")
 		localStorage.requisition = 1
+		
+	
 
-	if (	document.getElementById("Origem").value 		== "Origem" 		&&
-			document.getElementById("Descricao").value 	== "Descricao" 	&&
-			document.getElementById("Fabricante").value 	== "Fabricante" 	&&
-			document.getElementById("Tombamento").value 	== "Tombamento" 	&&
-			document.getElementById("Referencia").value 	== "Referencia" 	&&
-			document.getElementById("N. de serie").value == "N. de serie"	&&
-			document.getElementById("Peso").value 			== "Peso" 			&&
-			document.getElementById("Estado").value 		== "Estado" 		&&
-			document.getElementById("Valor").value 		== "Valor"				)
+	if (  document.getElementById("Origem").value 		== "Origem" 						&& 
+			document.getElementById("Descricao").value	== "Descricao" 					&&
+			document.getElementById("Fabricante").value	== "Fabricante" 					&& 
+			document.getElementById("Tombamento").value	== "Tombamento" 					&& 
+			document.getElementById("Referencia modelo").value	== "Referencia modelo" 	&& 
+			document.getElementById("N. de serie").value == "N. de serie" 					&& 
+			document.getElementById("Peso").value 			== "Peso" 							&& 
+			document.getElementById("Estado").value 		== "Estado" 						&& 
+			document.getElementById("Valor").value 		== "Valor")
 	{
 		verificar_dados()
 	}
 	else
 	{
-		localstorage.dataAq 			= document.getElementById("dataAq").value
-		localstorage.origem 			= document.getElementById("Origem").value
-		localstorage.categoria 		= document.getElementById("categoria").value
-		localstorage.grupo 			= (document.getElementById("categoria").value)?document.getElementById("grupo").value:"Não aplicável"
-		localstorage.descricao 		= document.getElementById("Descricao").value
-		localstorage.fabricante 	= document.getElementById("Fabricante").value
-		localstorage.tombamento 	= document.getElementById("Tombamento").value
-		localstorage.referencia 	= document.getElementById("Referencia").value
-		localstorage.serialN 		= document.getElementById("N. de serie").value
-		localstorage.peso 			= document.getElementById("Peso").value
-		localstorage.estado 			= document.getElementById("Estado").value
-		localstorage.valor 			= document.getElementById("Valor").value
-		localstorage.imagem 			= document.getElementById("imagem").value
+		localStorage.dataAq 			= document.getElementById("dataAq").value
+		localStorage.origem 			= document.getElementById("Origem").value
+		localStorage.categoria 		= document.getElementById("categoria").value
+		localStorage.grupo 			= (document.getElementById("categoria").value)?document.getElementById("grupo").value:"Não aplicável"
+		localStorage.descricao 		= document.getElementById("Descricao").value
+		localStorage.fabricante 	= document.getElementById("Fabricante").value
+		localStorage.tombamento 	= document.getElementById("Tombamento").value
+		localStorage.referencia 	= document.getElementById("Referencia modelo").value
+		localStorage.serialN 		= document.getElementById("N. de serie").value
+		localStorage.peso 			= document.getElementById("Peso").value
+		localStorage.estado 			= document.getElementById("Estado").value
+		localStorage.valor 			= document.getElementById("Valor").value
+		//localstorage.imagem 			= document.getElementById("imagem").value
 	}
+	
 
 }
 
